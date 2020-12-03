fde65a0c7ac1f767f586ec76210b6be54750b37d
hahahapro <75424113+hahahapro@users.noreply.github.com>
2020-12-03 12:55:05 +0200

    Add files via upload

---
+++ src/components/QuestionBox.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="question-box-container">
+    <b-jumbotron>
+       <template slot="lead">
+         {{ currentQuestion.question }}
+       </template>
+
+       <hr class="my-4">
+
+       <p v-for="(answer, index) in answers" :key="index">
+         {{ answer }}
+       </p>
+
+       <b-button variant="primary" href="#">Submit</b-button>
+       <b-button @click="next" variant="success">
+         Next
+       </b-button>
+    </b-jumbotron>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    currentQuestion: Object
+
+  }
+}
+</script>
